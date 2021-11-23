5aba9a5afbceb6d8344de29ecae1dc166fa4c80d
Connor Hughes <seanandhughes@gmail.com>
2021-11-23 02:07:03 +0000

    Adding queries group_by

---
+++ 3_queries_group-by/6_low_average_time_per_student.sql
@@ -0,0 +1,6 @@
+SELECT students.name as student_name, avg((avg(assignment_submissions.duration)) as estimated_average, avg(assignment_submissions.duration) as average_duration
+FROM students 
+JOIN assignment_submissions ON students.id = student_id
+WHERE end_date is NULL
+GROUP BY students.name
+ORDER BY average_duration DESC;
