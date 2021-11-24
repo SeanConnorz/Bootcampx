b0b997f02333739cb19ed3b82f7104a1f97868c3
Connor Hughes <seanandhughes@gmail.com>
2021-11-24 23:47:17 +0000

    Protecting site from injection attacks

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,15 +8,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+
+const querryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 ORDER BY teachers.name;
-`)
+`;
+
+const cohortName = process.argv[2];
+
+const values = [cohortName];
+
+pool.query(querryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
